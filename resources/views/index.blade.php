62d679298cb7bd99027f7a324e6343942916fbd2
Juliana Ferreira <juliana_ferreira91@hotmail.com>
2019-11-24 14:07:34 -0300

    Remoção de links desnecessários

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,40 +6,28 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SGIE - Teste PraValer - @yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/default.css') }}">
 </head>
 <body>
     <header>
         <nav class="navbar navbar-inverse">
             <div class="container-fluid">
                 <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="{{ url('/') }}">SGIE - Teste PraValer</a>
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="{{ url('/') }}">SGIE - Teste PraValer</a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="{{ url('/institutions') }}">Instituições<span class="sr-only">(current)</span></a></li>
-                    <li><a href="{{ url('/courses') }}">Cursos</a></li>
-                    <li><a href="{{ url('/students') }}">Alunos</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Link</a></li>
-                    <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
+                    <ul class="nav navbar-nav">
+                        <li><a href="{{ url('/institutions') }}">Instituições<span class="sr-only">(current)</span></a></li>
+                        <li><a href="{{ url('/courses') }}">Cursos</a></li>
+                        <li><a href="{{ url('/students') }}">Alunos</a></li>
                     </ul>
-                    </li>
-                </ul>
                 </div>
             </div>
         </nav>
